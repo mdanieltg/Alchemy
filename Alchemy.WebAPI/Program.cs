0ac86b3b4bd420416216d20eff5ec0a1be432532
Daniel Medrano Huerta <daniel@quicktortoise.com>
2024-09-08 17:33:17 -0600

    Add CORS policies

--- Alchemy.WebAPI/Program.cs
+++ Alchemy.WebAPI/Program.cs
@@ -32,6 +32,17 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "TES-V Alchemy WebService", Version = "v1" }));
 
+// Cors policies
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policyBuilder => policyBuilder
+        .WithOrigins("https://alchemy.quicktortoise.com.mx")
+        .WithMethods("GET"));
+    options.AddPolicy("development", policyBuilder => policyBuilder
+        .WithOrigins("http://localhost:4200")
+        .WithMethods("GET"));
+});
+
 WebApplication app = builder.Build();
 
 app.UseForwardedHeaders(new ForwardedHeadersOptions
@@ -42,11 +53,14 @@ app.UseForwardedHeaders(new ForwardedHeadersOptions
 // Configure HTTP req. pipeline
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
+    app.UseCors("development");
     app.UseSwagger();
     app.UseSwaggerUI(options =>
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "Alchemy.WebAPI v1"));
+
 }
+
+app.UseCors();
 
 app.UseAuthorization();
 
